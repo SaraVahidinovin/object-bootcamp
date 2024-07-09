c41d8bbdcb8f2be514304a21d35b804e1e616607
unknown <sa.vahidinovin@gmail.com>
2024-07-09 10:12:42 +0200

    exercise2

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ let book = {
     title: 'To Kill a Mockingbird',
     autor: 'Harper Lee',
     numPages: '281'
-}
+};
 
 //2:
+
+let books = [
+    {
+        title: 'To Kill a Mockingbird',
+        autor: 'Harper Lee',
+        numPages: '281'
+    },
+    {
+        title: 'Pride and Prejudice',
+        autor: 'Jane Austen',
+        numPages: '279'
+    },
+    {
+        title: 'The Great Gatsby',
+        autor: 'F. Scott Fitzgerald',
+        numPages: '180'   
+    }
+];
